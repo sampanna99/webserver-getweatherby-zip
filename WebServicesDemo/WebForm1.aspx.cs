8086af33ade43ab9b6046f49af79fec5934f4ea2
sampanna pokhrel <sampanna99@gmail.com>
2016-10-20 00:33:28 -0500

    first and all commit ..getting weather info by zip..timeout error on the proxy server.. Seems like it is working though.. Too many pools at the same time

---
+++ WebServicesDemo/WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace WebServicesDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGetWeather_Click(object sender, EventArgs e)
+        {
+            WeatherService.WeatherSoapClient client = new WeatherService.WeatherSoapClient("WeatherSoap");
+            WeatherService.WeatherReturn result = client.GetCityWeatherByZIP(txtZip.Text);
+
+            if (result.Success)
+            {
+                lblError.Text = string.Empty;
+                lblCity.Text = result.City;
+                lblState.Text = result.State;
+                lblTemperature.Text = result.Temperature;
+                lblWeatherStationCity.Text = result.WeatherStationCity;
+                lblWind.Text = result.Wind;
+            }
+            else
+            {
+                lblError.Text = result.ResponseText;
+                lblCity.Text = string.Empty;
+                lblState.Text = string.Empty;
+                lblTemperature.Text = string.Empty;
+                lblWeatherStationCity.Text = string.Empty;
+                lblWind.Text = string.Empty;
+            }
+        }
+    }
+}
